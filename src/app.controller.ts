4470ea820832e03a5e3f86f29f016d31408d2b74
viethoangkxtb <viethoangkxtb@gmail.com>
2025-02-07 23:25:03 +0700

    Connect Database

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,24 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Render } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ConfigService } from '@nestjs/config';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private configService: ConfigService,
+  ) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Render('home')
+  handleHomepage() {
+    console.log('>> check port = ', this.configService.get<string>('PORT'));
+
+    const message1 = this.appService.getHello();
+
+    // return this.appService.getHello();
+    return {
+      message: message1,
+    };
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    // MongooseModule.forRoot(
+    //   'mongodb+srv://viethoangkxtb:VcZQEK0TSFv6epN1@cluster0.zuo5n.mongodb.net/',
+    // ),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGO_URL'),
+      }),
+      inject: [ConfigService],
+    }),
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
