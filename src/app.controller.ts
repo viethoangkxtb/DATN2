e747626e13abccf919d0a6de6d3189b9136e1fde
viethoangkxtb <viethoangkxtb@gmail.com>
2025-02-17 16:48:00 +0700

    Enable authentication globally by Passport JWT

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,19 +1,25 @@
-import {Controller, Post, Request, UseGuards} from '@nestjs/common';
+import {Controller, Get, Post, Request, UseGuards} from '@nestjs/common';
 import {AppService} from './app.service';
 import {ConfigService} from '@nestjs/config';
-import {AuthGuard} from '@nestjs/passport';
 import { LocalAuthGuard } from './auth/local-auth.guard';
+import { AuthService } from './auth/auth.service';
+import { JwtAuthGuard } from './auth/jwt-auth.guard';
 
 @Controller()
 export class AppController {
   constructor(
     private readonly appService: AppService,
     private configService: ConfigService,
+    private authService: AuthService
   ) {}
 
-  @UseGuards(LocalAuthGuard)
   @Post('/login')
   handleLogin(@Request() req) {
+    return this.authService.login(req.user);
+  }
+
+  @Get('profile')
+  getProfile(@Request() req) {
     return req.user;
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { MongooseModule } from '@nestjs/mongoose';
-import { ConfigModule, ConfigService } from '@nestjs/config';
-import { UsersModule } from './users/users.module';
-import { AuthModule } from './auth/auth.module';
+import {Module} from '@nestjs/common';
+import {AppController} from './app.controller';
+import {AppService} from './app.service';
+import {MongooseModule} from '@nestjs/mongoose';
+import {ConfigModule, ConfigService} from '@nestjs/config';
+import {UsersModule} from './users/users.module';
+import {AuthModule} from './auth/auth.module';
+import { APP_GUARD } from '@nestjs/core';
+import { JwtAuthGuard } from './auth/jwt-auth.guard';
 
 @Module({
   imports: [
@@ -25,6 +27,12 @@ import { AuthModule } from './auth/auth.module';
     AuthModule,
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_GUARD,
+      useClass: JwtAuthGuard,
+    },
+  ],
 })
 export class AppModule {}
