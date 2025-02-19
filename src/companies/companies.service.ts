a5fa4207567a723d3f37530be35ee204cc816c3f
viethoangkxtb <viethoangkxtb@gmail.com>
2025-02-19 17:38:51 +0700

    Query with Pagination by page + limit

--- src/companies/companies.service.ts
+++ src/companies/companies.service.ts
@@ -6,6 +6,7 @@ import {InjectModel} from '@nestjs/mongoose';
 import {SoftDeleteModel} from 'soft-delete-plugin-mongoose';
 import {IUser} from 'src/users/user.interface';
 import mongoose from 'mongoose';
+import aqp from 'api-query-params';
 
 @Injectable()
 export class CompaniesService {
@@ -26,8 +27,33 @@ export class CompaniesService {
     return company;
   }
 
-  findAll() {
-    return `This action returns all companies`;
+  async findAll(currentPage: number, limit: number, qs: string) {
+    const {filter, sort, projection, population} = aqp(qs);
+    delete filter.page;
+    delete filter.limit;
+
+    let offset = (+currentPage - 1) * +limit;
+    let defaultLimit = +limit ? +limit : 10;
+    const totalItems = (await this.companyModel.find(filter)).length;
+    const totalPages = Math.ceil(totalItems / defaultLimit);
+    const result = await this.companyModel
+      .find(filter)
+      .skip(offset)
+      .limit(defaultLimit)
+      // @ts-ignore: Unreachable code error
+      .sort(sort)
+      .populate(population)
+      .exec();
+
+    return {
+      meta: {
+        current: currentPage, //trang hiện tại
+        pageSize: limit, //số lượng bản ghi đã lấy
+        pages: totalPages, //tổng số trang với điều kiện query
+        total: totalItems, // tổng số phần tử (số bản ghi)
+      },
+      result, //kết quả query
+    };
   }
 
   findOne(id: string) {
