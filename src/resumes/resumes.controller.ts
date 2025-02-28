861f2cadeccd5c2773d1e48a64b3dd66516b40c2
viethoangkxtb <viethoangkxtb@gmail.com>
2025-02-28 22:56:30 +0700

    Fetch all resume with pagination

--- src/resumes/resumes.controller.ts
+++ src/resumes/resumes.controller.ts
@@ -6,6 +6,7 @@ import {
   Patch,
   Param,
   Delete,
+  Query,
 } from '@nestjs/common';
 import {ResumesService} from './resumes.service';
 import {CreateResumeDto, CreateUserCvDto} from './dto/create-resume.dto';
@@ -23,10 +24,15 @@ export class ResumesController {
     return this.resumesService.create(createUserCvDto, user);
   }
 
-  @Get()
-  findAll() {
-    return this.resumesService.findAll();
-  }
+  @ResponseMessage('Fetch Company list with pagination')
+    @Get()
+    findAll(
+      @Query('current') currentPage: string,
+      @Query('pageSize') limit: string,
+      @Query() qs: string,
+    ) {
+      return this.resumesService.findAll(+currentPage, +limit, qs);
+    }
 
   @Get(':id')
   findOne(@Param('id') id: string) {
--- src/resumes/resumes.service.ts
+++ src/resumes/resumes.service.ts
@@ -6,6 +6,7 @@ import {InjectModel} from '@nestjs/mongoose';
 import {Resume, ResumeDocument} from './schemas/resume.schema';
 import {SoftDeleteModel} from 'soft-delete-plugin-mongoose';
 import mongoose from 'mongoose';
+import aqp from 'api-query-params';
 
 @Injectable()
 export class ResumesService {
@@ -44,8 +45,32 @@ export class ResumesService {
     };
   }
 
-  findAll() {
-    return `This action returns all resumes`;
+  async findAll(currentPage: number, limit: number, qs: string) {
+    const {filter, sort, projection, population} = aqp(qs);
+    delete filter.current;
+    delete filter.pageSize;
+
+    let offset = (+currentPage - 1) * +limit;
+    let defaultLimit = +limit ? +limit : 10;
+    const totalItems = (await this.resumeModel.find(filter)).length;
+    const totalPages = Math.ceil(totalItems / defaultLimit);
+    const result = await this.resumeModel
+      .find(filter)
+      .skip(offset)
+      .limit(defaultLimit)
+      .sort(sort as any)
+      .populate(population)
+      .exec();
+
+    return {
+      meta: {
+        current: currentPage, //trang hiện tại
+        pageSize: limit, //số lượng bản ghi đã lấy
+        pages: totalPages, //tổng số trang với điều kiện query
+        total: totalItems, // tổng số phần tử (số bản ghi)
+      },
+      result, //kết quả query
+    };
   }
 
   findOne(id: number) {
