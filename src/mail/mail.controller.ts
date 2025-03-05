baeb360dc6562f3048373602ffb7dec264d08032
viethoangkxtb <viethoangkxtb@gmail.com>
2025-03-05 15:29:38 +0700

    Test send mail

---
+++ src/mail/mail.controller.ts
@@ -0,0 +1,24 @@
+import {Controller, Get} from '@nestjs/common';
+import {MailService} from './mail.service';
+import {Public, ResponseMessage} from 'src/decorator/customize';
+import {MailerService} from '@nestjs-modules/mailer';
+
+@Controller('mail')
+export class MailController {
+  constructor(
+    private readonly mailService: MailService,
+    private mailerService: MailerService,
+  ) {}
+
+  @ResponseMessage('Test email')
+  @Get()
+  @Public()
+  async handleTestEmail() {
+    await this.mailerService.sendMail({
+      to: 'viethoangkxtb@gmail.com',
+      from: '"Support Team" <support@example.com>', // override default from
+      subject: 'Welcome to Nice App! Confirm your Email',
+      html: '<b>welcome bla bla</b>', // HTML body content
+    });
+  }
+}
