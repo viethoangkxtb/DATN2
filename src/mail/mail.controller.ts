6b642302ba9cc6e146bca66f82ac127a4230b014
viethoangkxtb <viethoangkxtb@gmail.com>
2025-03-05 15:50:54 +0700

    Send email with template

--- src/mail/mail.controller.ts
+++ src/mail/mail.controller.ts
@@ -18,7 +18,7 @@ export class MailController {
       to: 'viethoangkxtb@gmail.com',
       from: '"Support Team" <support@example.com>', // override default from
       subject: 'Welcome to Nice App! Confirm your Email',
-      html: '<b>welcome bla bla</b>', // HTML body content
+      template: 'job',
     });
   }
 }
--- src/mail/mail.module.ts
+++ src/mail/mail.module.ts
@@ -3,6 +3,8 @@ import {MailService} from './mail.service';
 import {MailController} from './mail.controller';
 import {ConfigService} from '@nestjs/config';
 import {MailerModule} from '@nestjs-modules/mailer';
+import {join} from 'path';
+import {HandlebarsAdapter} from '@nestjs-modules/mailer/dist/adapters/handlebars.adapter';
 
 @Module({
   imports: [
@@ -17,13 +19,15 @@ import {MailerModule} from '@nestjs-modules/mailer';
           },
         },
 
-        // template: {
-        //   dir: join(__dirname, 'templates'),
-        //   adapter: new HandlebarsAdapter(),
-        //   options: {
-        //     strict: true,
-        //   },
-        // },
+        template: {
+          dir: join(__dirname, 'templates'),
+          adapter: new HandlebarsAdapter(),
+          options: {
+            strict: true,
+          },
+        },
+
+        preview: true,
       }),
       inject: [ConfigService],
     }),
