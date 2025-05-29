346260d4256ce8473fbac52700d5bfc817fbddd3
viethoangkxtb <viethoangkxtb@gmail.com>
2025-05-29 23:30:18 +0700

    adress (user.schema) => address

--- src/users/dto/update-user.dto.ts
+++ src/users/dto/update-user.dto.ts
@@ -4,6 +4,7 @@ import {IsNotEmpty} from 'class-validator';
 
 export class UpdateUserDto extends OmitType(CreateUserDto, [
   `password`,
+  `email`,
 ] as const) {
   // @IsNotEmpty({message: "_id is required"})
   // _id: string;
--- src/users/schemas/user.schema.ts
+++ src/users/schemas/user.schema.ts
@@ -22,7 +22,7 @@ export class User {
   gender: string;
 
   @Prop()
-  adress: string;
+  address: string;
 
   @Prop()
   phone: string;
